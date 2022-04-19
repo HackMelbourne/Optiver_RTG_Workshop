5d08ba54475e7719131c6e7194b11a8952b09edf
Ryan <ryan2002.rs@gmail.com>
2022-04-19 11:38:23 +1000

    Add very basic (but better) autotrader

--- autotrader.py
+++ autotrader.py
@@ -22,7 +22,6 @@ from typing import List
 
 from ready_trader_go import BaseAutoTrader, Instrument, Lifespan, MAXIMUM_ASK, MINIMUM_BID, Side
 
-
 LOT_SIZE = 10
 POSITION_LIMIT = 100
 TICK_SIZE_IN_CENTS = 100
@@ -77,12 +76,41 @@ class AutoTrader(BaseAutoTrader):
         prices are reported along with the volume available at each of those
         price levels.
         """
+        vwap_buy = (sum(
+            map(lambda x, y: x * y, bid_prices[:1], bid_volumes[:1]))) / sum(bid_volumes[:1]) if sum(bid_volumes[:1]) > 0 else 0
+        vwap_sell = (sum(
+            map(lambda x, y: x * y, ask_prices[:1], ask_volumes[:1]))) / sum(ask_volumes[:1]) if sum(ask_volumes[:1]) > 0 else 0
+        print(vwap_buy, vwap_sell)
         self.logger.info("received order book for instrument %d with sequence number %d", instrument,
                          sequence_number)
-        if instrument == Instrument.FUTURE:
-            price_adjustment = - (self.position // LOT_SIZE) * TICK_SIZE_IN_CENTS
-            new_bid_price = bid_prices[0] + price_adjustment if bid_prices[0] != 0 else 0
-            new_ask_price = ask_prices[0] + price_adjustment if ask_prices[0] != 0 else 0
+        # if instrument == Instrument.FUTURE:
+        #     price_adjustment = - (self.position // LOT_SIZE) * TICK_SIZE_IN_CENTS
+        #     new_bid_price = bid_prices[0] + price_adjustment if bid_prices[0] != 0 else 0
+        #     new_ask_price = ask_prices[0] + price_adjustment if ask_prices[0] != 0 else 0
+        #
+        #     if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
+        #         self.send_cancel_order(self.bid_id)
+        #         self.bid_id = 0
+        #     if self.ask_id != 0 and new_ask_price not in (self.ask_price, 0):
+        #         self.send_cancel_order(self.ask_id)
+        #         self.ask_id = 0
+        #
+        #     if self.bid_id == 0 and new_bid_price != 0 and self.position < POSITION_LIMIT:
+        #         self.bid_id = next(self.order_ids)
+        #         self.bid_price = new_bid_price
+        #         self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
+        #         self.bids.add(self.bid_id)
+        #
+        #     if self.ask_id == 0 and new_ask_price != 0 and self.position > -POSITION_LIMIT:
+        #         self.ask_id = next(self.order_ids)
+        #         self.ask_price = new_ask_price
+        #         self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
+        #         self.asks.add(self.ask_id)
+
+        if instrument == Instrument.ETF:
+            new_bid_price = int(vwap_buy/TICK_SIZE_IN_CENTS) * TICK_SIZE_IN_CENTS
+            new_ask_price = int(vwap_sell/TICK_SIZE_IN_CENTS) * TICK_SIZE_IN_CENTS
+            # theo=int(((new_bid_price+new_ask_price)/2)//TICK_SIZE_IN_CENTS*TICK_SIZE_IN_CENTS)
 
             if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
                 self.send_cancel_order(self.bid_id)
@@ -91,17 +119,19 @@ class AutoTrader(BaseAutoTrader):
                 self.send_cancel_order(self.ask_id)
                 self.ask_id = 0
 
-            if self.bid_id == 0 and new_bid_price != 0 and self.position < POSITION_LIMIT:
+            if self.bid_id == 0 and new_bid_price != 0 and self.position+LOT_SIZE < POSITION_LIMIT:
                 self.bid_id = next(self.order_ids)
                 self.bid_price = new_bid_price
                 self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
                 self.bids.add(self.bid_id)
 
-            if self.ask_id == 0 and new_ask_price != 0 and self.position > -POSITION_LIMIT:
+            if self.ask_id == 0 and new_ask_price != 0 and self.position-LOT_SIZE > -POSITION_LIMIT:
                 self.ask_id = next(self.order_ids)
                 self.ask_price = new_ask_price
                 self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
                 self.asks.add(self.ask_id)
+
+
 
     def on_order_filled_message(self, client_order_id: int, price: int, volume: int) -> None:
         """Called when when of your orders is filled, partially or fully.
